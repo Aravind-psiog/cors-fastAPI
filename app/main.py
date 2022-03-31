99256d53c7883897f082f7b52dea5f1cf0eb62d3
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-03-31 11:21:22 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import json
 
 
@@ -8,30 +9,35 @@ import json
 # app = FastAPI(root_path="/dev/")
 app = FastAPI()
 
-# origins = [
-#     "*"
-# ]
+origins = [
+    "*"
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
 def root():
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        },
-        'body': json.dumps('Hello from Lambda!')
-    }
+    # return {
+    #     'statusCode': 200,
+    #     'headers': {
+    #         'Access-Control-Allow-Headers': 'Content-Type',
+    #         'Access-Control-Allow-Origin': '*',
+    #         'Access-Control-Allow-Methods': '*'
+    #     },
+    #     'body': json.dumps('Hello from Lambda!')
+    # }
+    content = {"message": "Hello World"}
+    headers = {'Access-Control-Allow-Headers': 'Content-Type',
+               'Access-Control-Allow-Origin': '*',
+               'Access-Control-Allow-Methods': '*'}
+    return JSONResponse(content=content, headers=headers)
 
 
 handler = Mangum(app)
