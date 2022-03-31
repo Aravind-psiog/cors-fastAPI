cb2f6e6f77354fe6f501f5979b53fb6b2996bb34
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-03-31 10:51:29 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -8,17 +8,17 @@ import json
 # app = FastAPI(root_path="/dev/")
 app = FastAPI()
 
-origins = [
-    "*"
-]
+# origins = [
+#     "*"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get('/')
